2f850f112a3af14371339a52c316ba2e06c58e3d
jerry6475 <jerry6475@naver.com>
2024-12-02 15:49:30 +0900

    1202 수업내용

--- day01/DocTest.java
+++ day01/DocTest.java
@@ -1,12 +1,6 @@
 package day01;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 
-@Getter
-@Setter
-@Slf4j
 public class DocTest {
     public static void main(String[] args) {
 
