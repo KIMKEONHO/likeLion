7822881f85e521566077eaccbf39e52c9678dfdc
jerry6475 <jerry6475@naver.com>
2024-12-02 15:48:53 +0900

    1202 수업내용

---
+++ day01/DocTest.java
@@ -0,0 +1,16 @@
+package day01;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+@Slf4j
+public class DocTest {
+    public static void main(String[] args) {
+
+        // 아무거나 테스트
+        System.out.printf("주석문 안에 들어있는 것들은 실행 x");
+    }
+}
---
+++ day01/Rectangle.java
@@ -0,0 +1,33 @@
+package day01;
+
+public class Rectangle {
+
+    public static void main(String[] args) {
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println("**********");
+//        }
+
+//        for (int i = 1; i <= 10; i++) {
+//            for (int j = 1; j <= 10; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//        System.out.println("**********");
+//        System.out.println("**********");
+//        System.out.println("**********");
+//        System.out.println("**********");
+//        System.out.println("**********");
+//        System.out.println("**********");
+//        System.out.println("**********");
+//        System.out.println("**********");
+//        System.out.println("**********");
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println("**********");
+            i++;
+        }
+    }
+}
