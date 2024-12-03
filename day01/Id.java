99fe34bdca595a7f6c3f67959343ada1dc7c739d
jerry6475 <jerry6475@naver.com>
2024-12-03 15:50:25 +0900

    1203 수업내용

---
+++ day01/Id.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class Id {
+    static private String name = "김건호";
+    static private String gender = "남자";
+    static private String email = "jerry6475@naver.com";
+    public static void main(String[] args) {
+        System.out.println(Id.name);
+        System.out.println(Id.gender);
+        System.out.println(Id.email);
+    }
+}
---
+++ day02/ValTest.java
@@ -0,0 +1,25 @@
+package day02;
+
+public class ValTest {
+    public static void main(String[] args) {
+        int a = 10;
+
+        byte b = 122;
+        short c = 10;
+        int d  = 10;
+        long e = 16;
+
+        float f = 1.0f;
+        double g = 1.0;
+
+        char h = 'a';
+        boolean j = true;
+
+//        j = b > 200 || a++ > 5;
+        System.out.println(j);;
+//        for(int i = 0; i < 100; i++){
+//            System.out.println(i+1);
+//        }
+
+    }
+}
