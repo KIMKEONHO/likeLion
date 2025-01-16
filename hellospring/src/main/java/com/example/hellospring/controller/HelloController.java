ddfcb69d78f45c885aaa0904c2090a9a64cc5306
jerry6475 <jerry6475@naver.com>
2025-01-16 16:52:14 +0900

    01-16 수업활동

---
+++ hellospring/src/main/java/com/example/hellospring/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.hellospring.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/")
+    public String hi(){
+        return "hi";
+    }
+}
