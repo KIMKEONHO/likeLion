8a6be849a45b0558a9c997eafe1bd6552bd322fb
jerry6475 <jerry6475@naver.com>
2025-01-24 16:10:20 +0900

    0124 수업내용(유효성 검사 컨트롤러에서 사용법)

--- springmvc/src/main/java/com/example/springmvc/controller/UserController.java
+++ springmvc/src/main/java/com/example/springmvc/controller/UserController.java
@@ -1,15 +1,26 @@
 package com.example.springmvc.controller;
 
 import com.example.springmvc.domain.User;
+import jakarta.validation.Valid;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class UserController {
+    @ModelAttribute
+    public void addAttributes(Model model){
+        System.out.println("@ModelAttribute 메소드 실행!!!! :::::::::");
+        model.addAttribute("msg","환영합니다^^");
+    }
+
     @GetMapping("/users")
     public String users(Model model) {
         List<User> users = Arrays.asList(
@@ -22,4 +33,19 @@ public class UserController {
 
         return "users";
     }
+    @GetMapping("register")
+    public String registerForm(Model model){
+        model.addAttribute("user", new User());
+
+        return "registerForm";
+    }
+
+    @PostMapping("register")
+    public String register(@Valid @ModelAttribute("user") User user, BindingResult result){
+        if(result.hasErrors())
+            return "registerForm";
+
+        return "result";
+    }
+
 }
