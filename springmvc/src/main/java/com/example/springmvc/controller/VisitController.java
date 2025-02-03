c2f37a29418a331acdea107f8e7087debc4edf73
jerry6475 <jerry6475@naver.com>
2025-02-03 16:24:42 +0900

    0203 수업내용(Cookie)

--- springmvc/src/main/java/com/example/springmvc/controller/VisitController.java
+++ springmvc/src/main/java/com/example/springmvc/controller/VisitController.java
@@ -1,6 +1,7 @@
 package com.example.springmvc.controller;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,8 +13,17 @@ public class VisitController {
 
     @GetMapping("/visit")
     public String visit(@CookieValue(name = "lastVisit", defaultValue = "N/A") String lastVisit,
-            HttpServletResponse response, Model model
+            HttpServletResponse response, Model model, HttpServletRequest request
     ) {
+
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for(Cookie cookie : cookies) {
+                String name = cookie.getName();
+                String value = cookie.getValue();
+                System.out.println(name + ":::" + value);
+            }
+        }
 
         Cookie cookie = new Cookie("lastVisit", "KEONHO");
         // / 주소에서 쿠키값을 사용 가능하게 설정
@@ -25,5 +35,30 @@ public class VisitController {
         response.addCookie(cookie);
        model.addAttribute("lastVisit", lastVisit);
        return "visit";
+    }
+
+    @GetMapping("/cookieSetForm")
+    public String cookieSetForm() {
+        // 쿠키 이름과 값을 받을 화면을 리턴
+        return "cookieset";
+    }
+
+    @GetMapping("/cookieSet")
+    public String cookieSet(HttpServletRequest request, HttpServletResponse response) {
+        String cookieName = request.getParameter("cookieName");
+        String cookieValue = request.getParameter("cookieValue");
+
+        Cookie cookie = new Cookie(cookieName, cookieValue);
+        cookie.setMaxAge(60 * 60 * 24); // 1일 동안 유지
+        response.addCookie(cookie);
+
+        return "redirect:/cookieView";
+    }
+
+    @GetMapping("/cookieView")
+    public String cookieView(HttpServletRequest request, Model model) {
+        Cookie[] cookies = request.getCookies();
+        model.addAttribute("cookies", cookies);
+        return "cookieview"; // 쿠키를 보여주는 화면
     }
 }
